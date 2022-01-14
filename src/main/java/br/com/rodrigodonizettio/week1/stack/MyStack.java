c084ee24dc12b6cef29e00c132d6d6e25b51954a
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2022-01-14 07:31:41 -0300

    Implementing MyStack class

---
+++ src/main/java/br/com/rodrigodonizettio/week1/stack/MyStack.java
@@ -0,0 +1,43 @@
+package br.com.rodrigodonizettio.week1.stack;
+
+import java.util.Stack;
+
+public class MyStack extends Stack<Integer> {
+    private int maxSize;
+
+    public MyStack(Integer maxSize) {
+        if(maxSize <= 0) {
+            throw new IllegalArgumentException("MaxSize cannot be <= 0");
+        }
+        this.maxSize = maxSize;
+    }
+
+
+    public boolean isEmpty() {
+        return super.isEmpty();
+    }
+
+    public int size() {
+        return super.size();
+    }
+
+    public Integer push(Integer object) {
+        if(this.size() >= maxSize) {
+            throw new ArrayIndexOutOfBoundsException("Stack is full. MaxSize is " + maxSize);
+        }
+        super.push(object);
+        return object;
+    }
+
+    public Integer pop() {
+        return super.pop();
+    }
+
+    public void popAll() {
+        super.removeAllElements();
+    }
+
+    public Integer top() {
+        return super.peek();
+    }
+}
