00f37737fac33563b8e3ee208535327826bc1f45
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2022-01-14 08:12:03 -0300

    Implementing CamelCaseUtil test for Empty String- 2nd cycle of TDD (breaks)
    Moreover,
    - CamelCaseUtil class does not throw IllegalArgumentException.

--- src/test/java/br/com/rodrigodonizettio/week1/camelcase/CamelCaseUtilTest.java
+++ src/test/java/br/com/rodrigodonizettio/week1/camelcase/CamelCaseUtilTest.java
@@ -2,6 +2,7 @@ package br.com.rodrigodonizettio.week1.camelcase;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CamelCaseUtilTest {
@@ -9,5 +10,12 @@ public class CamelCaseUtilTest {
     void assertNullStringWillThrowNullPointerExceptionTest() {
         String input = null;
         assertThrows(NullPointerException.class, () -> CamelCaseUtil.convertToStringList(input));
+    }
+
+    @Test
+    void assertEmptyStringWillThrowIllegalArgumentExceptionTest() {
+        String input = "";
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> CamelCaseUtil.convertToStringList(input));
+        assertEquals("String is Blank!", illegalArgumentException.getMessage());
     }
 }
