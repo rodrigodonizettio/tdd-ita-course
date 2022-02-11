7190e6263f561f636a381425b2e877fa86a42f0d
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2022-02-11 09:07:22 -0300

    Adding private constructor to CamelCaseUtil class to override implicit public one

--- src/main/java/br/com/rodrigodonizettio/week1/camelcase/CamelCaseUtil.java
+++ src/main/java/br/com/rodrigodonizettio/week1/camelcase/CamelCaseUtil.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class CamelCaseUtil {
+    private CamelCaseUtil() { }
 
     public static List<String> convertToStringList(String input) {
         performThrowableExceptionsValidations(input);
