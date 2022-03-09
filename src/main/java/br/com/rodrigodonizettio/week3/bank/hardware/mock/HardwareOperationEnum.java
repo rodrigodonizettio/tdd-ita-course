2610d7ecc64f532fe251785696bfeca4b31ed013
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2022-03-09 09:04:06 -0300

    Implementing Hardware Operation Enum

---
+++ src/main/java/br/com/rodrigodonizettio/week3/bank/hardware/mock/HardwareOperationEnum.java
@@ -0,0 +1,9 @@
+package br.com.rodrigodonizettio.week3.bank.hardware.mock;
+
+public enum HardwareOperationEnum {
+    LOGIN(),
+    WITHDRAW(),
+    DEPOSIT();
+
+    HardwareOperationEnum() { }
+}
