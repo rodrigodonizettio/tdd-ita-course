5e108d65af74f6ee3618ba496a26e041f7cf45e0
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2022-03-14 08:06:03 -0300

    Implementing video shop Rental class

---
+++ src/main/java/br/com/rodrigodonizettio/week4/videoshop/Rental.java
@@ -0,0 +1,19 @@
+package br.com.rodrigodonizettio.week4.videoshop;
+
+public class Rental {
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+}
