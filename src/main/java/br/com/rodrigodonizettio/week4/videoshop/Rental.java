0359c4dee7f3e3d41b7770ffabe5e3988e75315d
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2022-03-17 07:51:21 -0300

    Implementing 'getAmount' and 'getFrequentRenterPoints' methods at Rental class

--- src/main/java/br/com/rodrigodonizettio/week4/videoshop/Rental.java
+++ src/main/java/br/com/rodrigodonizettio/week4/videoshop/Rental.java
@@ -1,5 +1,7 @@
 package br.com.rodrigodonizettio.week4.videoshop;
 
+import br.com.rodrigodonizettio.week4.videoshop.movie.Movie;
+
 public class Rental {
     private Movie movie;
     private int daysRented;
@@ -15,5 +17,13 @@ public class Rental {
 
     public int getDaysRented() {
         return daysRented;
+    }
+
+    public int getFrequentRenterPoints() {
+        return movie.getFrequentRenterPoints(daysRented);
+    }
+
+    public double getAmount() {
+        return movie.getAmount(daysRented);
     }
 }
