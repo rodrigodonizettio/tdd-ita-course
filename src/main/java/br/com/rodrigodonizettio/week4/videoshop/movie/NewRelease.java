dac5f4ba92d9f87406f11ff377730eb97737e086
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2022-03-17 07:48:36 -0300

    Implementing NewRelease Movie subclass

---
+++ src/main/java/br/com/rodrigodonizettio/week4/videoshop/movie/NewRelease.java
@@ -0,0 +1,20 @@
+package br.com.rodrigodonizettio.week4.videoshop.movie;
+
+public class NewRelease extends Movie {
+    public NewRelease(String title) {
+        super(title);
+    }
+
+    @Override
+    public double getAmount(int daysRented) {
+        return daysRented * 3;
+    }
+
+    @Override
+    public int getFrequentRenterPoints(int daysRented) {
+        if(daysRented > 1) {
+            return 2;
+        }
+        return 1;
+    }
+}
